2753a5a4e7dd43d88d95563c32a92961e833ed56
Fábio Alves Baptista Filho <136496341+fabiozz@users.noreply.github.com>
2024-05-15 21:10:12 -0300

    Add files via upload

---
+++ models/jogo.java
@@ -0,0 +1,65 @@
+import java.util.List;
+
+public class Jogo {
+    private String titulo;
+    private Desenvolvedor desenvolvedor;
+    private int ano;
+    private double valor;
+    private List<String> categorias;
+
+    public Jogo(String titulo, Desenvolvedor desenvolvedor, int ano, double valor, List<String> categorias) {
+        this.titulo = titulo;
+        this.desenvolvedor = desenvolvedor;
+        this.ano = ano;
+        this.valor = valor;
+        this.categorias = categorias;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Desenvolvedor getDesenvolvedor() {
+        return desenvolvedor;
+    }
+
+    public void setDesenvolvedor(Desenvolvedor desenvolvedor) {
+        this.desenvolvedor = desenvolvedor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public List<String> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<String> categorias) {
+        this.categorias = categorias;
+    }
+
+    public void jogar() {
+        System.out.println("Jogando " + titulo);
+    }
+  
+    public void type() {
+  
+    }
+}
