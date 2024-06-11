3b28651a765349d8440bc3f7df13bd3a78d93c03
Fábio Alves Baptista Filho <136496341+fabiozz@users.noreply.github.com>
2024-06-11 14:50:32 -0300

    add: backend library and shopping cart

--- src/main/java/com/example/demo/entity/Biblioteca.java
+++ src/main/java/com/example/demo/entity/Biblioteca.java
@@ -1,10 +1,14 @@
 package com.example.demo.entity;
 
-import com.example.demo.entity.Jogo;
-import jakarta.persistence.*;
-
+import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 @Entity
 public class Biblioteca {
 
@@ -12,8 +16,26 @@ public class Biblioteca {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "biblioteca", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Jogo> jogos;
 
-    // getters e setters
+    public Biblioteca() {
+        this.jogos = new ArrayList<>();
+    }
+    public List<Jogo> getJogos() {
+        return jogos;
+    }
+
+    public void setJogos(List<Jogo> jogos) {
+        this.jogos = jogos;
+    }
+
+    public void adicionarJogo(Jogo jogo) {
+        jogos.add(jogo);
+    }
+
+    public void removerJogo(Jogo jogo) {
+        jogos.remove(jogo);
+    }
+
 }
--- src/main/java/com/example/demo/entity/CarrinhoDeCompras.java
+++ src/main/java/com/example/demo/entity/CarrinhoDeCompras.java
@@ -1,7 +1,14 @@
 package com.example.demo.entity;
-import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class CarrinhoDeCompras {
@@ -10,8 +17,41 @@ public class CarrinhoDeCompras {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Jogo> jogos;
 
-    // getters e setters
+    public CarrinhoDeCompras() {
+        this.jogos = new ArrayList<>();
+    }
+
+    public List<Jogo> getJogos() {
+        return jogos;
+    }
+
+    public void setJogos(List<Jogo> jogos) {
+        this.jogos = jogos;
+    }
+
+    public void adicionarProduto(Jogo jogo) {
+        jogos.add(jogo);
+    }
+
+    public void removerProduto(Jogo jogo) {
+        jogos.remove(jogo);
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+        for (Jogo jogo : jogos) {
+            total += jogo.getValor();
+        }
+        return total;
+    }
+
+    public void exibirProdutos() {
+        System.out.println("Produtos no carrinho:");
+        for (Jogo jogo : jogos) {
+            System.out.println(jogo.getTitulo() + " - R$" + jogo.getValor());
+        }
+    }
 }
